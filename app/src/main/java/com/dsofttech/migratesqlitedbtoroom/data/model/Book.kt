7220339ab0dbc786e18bf7cef4d9ec15caf03df9
D-Soft-Tech <dynamicsofttechnology@gmail.com>
2023-07-17 19:50:13 +0100

    Updated the data class to use Room's annotations

--- app/src/main/java/com/dsofttech/migratesqlitedbtoroom/data/model/Book.kt
+++ app/src/main/java/com/dsofttech/migratesqlitedbtoroom/data/model/Book.kt
@@ -1,8 +1,23 @@
 package com.dsofttech.migratesqlitedbtoroom.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.dsofttech.migratesqlitedbtoroom.utils.AppConstants.BOOK_AUTHOR
+import com.dsofttech.migratesqlitedbtoroom.utils.AppConstants.BOOK_ID
+import com.dsofttech.migratesqlitedbtoroom.utils.AppConstants.BOOK_TITLE
+import com.dsofttech.migratesqlitedbtoroom.utils.AppConstants.NUM_OF_PAGES
+import com.dsofttech.migratesqlitedbtoroom.utils.AppConstants.TABLE_NAME
+
+@Entity(tableName = TABLE_NAME)
 data class Book(
+    @ColumnInfo(name = BOOK_ID)
+    @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
+    @ColumnInfo(name = BOOK_TITLE)
     val title: String,
+    @ColumnInfo(name = BOOK_AUTHOR)
     val author: String,
+    @ColumnInfo(name = NUM_OF_PAGES)
     val numOfPages: Int,
 )
---
+++ app/src/main/java/com/dsofttech/migratesqlitedbtoroom/utils/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.dsofttech.migratesqlitedbtoroom.utils
+
+object AppConstants {
+    const val DB_NAME = "book_db"
+    const val TABLE_NAME = "testBook"
+    const val BOOK_ID: String = "bookId"
+    const val BOOK_TITLE: String = "bookTitle"
+    const val BOOK_AUTHOR: String = "bookAuthor"
+    const val NUM_OF_PAGES: String = "numOfPages"
+}
